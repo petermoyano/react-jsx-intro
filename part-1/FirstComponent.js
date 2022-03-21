ef387f970b3928703cca6d43323ed46a0f72b896
Pedro Moyano <pedromoyano454@gmail.com>
2022-03-21 13:15:42 -0300

    part one

---
+++ part-1/FirstComponent.js
@@ -0,0 +1,4 @@
+function FirstComponent(){
+    return <h1> My first component! </h1>
+    }
+    
---
+++ part-1/NamedComp.js
@@ -0,0 +1,6 @@
+
+// renders a p that should accept a property of “name” and display text “My name is name”.
+
+function NamedComp({ myName }) {
+    return (<p> My name is {myName} </p>);
+};
